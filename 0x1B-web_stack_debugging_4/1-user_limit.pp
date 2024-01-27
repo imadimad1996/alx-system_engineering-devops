0391aed2264bddc7bc539a03f83d4289659091e5
imad1996@A <imadazizimad@gmail.com>
2024-01-27 16:59:53 +0000

    0x1B-web_stack_debugging_4

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,5 @@
-# Fix problem of high amount of requests
-
-exec {'replace':
+#change ULIMIT
+exec { 'Change ULIMIT':
+  command  => 'echo -e "holberton hard nofile 2500\nholberton soft nofile 25000" > /etc/security/limits.conf',
   provider => shell,
-  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
-  before   => Exec['restart'],
-}
-
-exec {'restart':
-  provider => shell,
-  command  => 'sudo service nginx restart',
 }
